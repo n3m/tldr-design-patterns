2308a5e606b13b721aec169808fb20b3812d2ae5
Alan Maldonado <business.alanm@gmail.com>
2022-07-12 23:27:06 -0500

    Factory Method

--- creational-patterns/factory-method/golang/application.go
+++ creational-patterns/factory-method/golang/application.go
@@ -4,8 +4,25 @@ import (
 	"golang/logistics"
 )
 
+/*
+	CONTEXT:
+ In a Factory Method pattern, the whole point is to be able to generate instances with different
+ properties that are property of the specific type, but also the ones that are from the parent.
+
+ In our case, we're attepting to generate a new transport, and specifically we want a bus, so at
+ the end, our variable type will be a Bus of some sorts
+*/
+
+/*
+ SOLUTION:
+ Go to file > logistics/main.go for further explanation
+*/
+
 func main() {
 
-	var product *logistics.Product = nil
+	// We're creating a new transport, and we're passing the type of transport we want to generate
+	someTransport := logistics.CreateNewTransport("bus")
+
+	/* bla bla bla */
 
 }
--- creational-patterns/factory-method/golang/logistics/main.go
+++ creational-patterns/factory-method/golang/logistics/main.go
@@ -1,39 +1,54 @@
 package logistics
 
-/* Interfaces */
-type Product interface {
-	Use()
+/*
+	HOW-TO: Factory Method Pattern
+*/
+
+/*
+	CONTEXT:
+	In this specific case, we're using Go's interfaces in order to generate an Object-Oriented implementation hierarchy
+*/
+
+// This interface will represent the father of the "category" of objects that we want to generate
+type ILogisticTransport interface {
+	Deliver()
+	Load()
 }
 
-type ProductCreator interface {
-	CreateProduct() Product
+// This struct will generate all the baseline implementations of the interface, and we will derive all
+// of our other objects from this
+type transport struct {
 }
 
-/* Products */
-type Toolbox struct{}
+func (t *transport) Deliver() {
 
-func (t *Toolbox) Use() {}
-
-type Bottle struct{}
-
-func (t *Bottle) Use() {}
-
-/* Creators */
-
-type ToolboxCreator struct {
-	info string
 }
 
-func (t *ToolboxCreator) CreateProduct() *Toolbox {
-	return &Toolbox{}
+func (t *transport) Load() {
+
 }
 
-type BottleCreator struct {
-	info string
+// In the following lines we're deriving a Bus and a Ship structure that inherits the transport structure properties and that also implements the ILogisticTransport interface
+type Bus struct {
+	transport
 }
 
-func (t *BottleCreator) CreateProduct() *Bottle {
-	return &Bottle{}
+type Ship struct {
+	transport
 }
 
-/* Creator Functions*/
+// This method will be in charge of being our "Creator"
+// Basically we just need to pass some kind of type to this method,
+// and it will return a new instance of an object that we want to generate
+// that meets the interface requirements
+func CreateNewTransport(t string) ILogisticTransport {
+	switch t {
+	case "bus":
+		return &Bus{}
+	case "ship":
+		return &Ship{}
+	default:
+		return nil
+	}
+
+}
